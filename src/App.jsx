0277b41eed0e65224cfa6e0f8d2d9649281617ab
sannishamusideen <samsonanu@yahoo.com>
2023-12-04 10:33:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testpage from './components/Testpage'
+
+const App = () => {
+  return (
+    <div>
+      <Testpage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Testpage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Testpage = () => {
+  return (
+    <div>
+      Hello World
+      <ul>
+        <li>Book</li>
+        <li>Book</li>
+        <li>Book</li>
+        <li>Book</li>
+        <li>Book</li>
+        <li>Book</li>
+        <li>Look</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Testpage;
